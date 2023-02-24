15be39716e492acd83ec1026a5867252d7e34736
Joseph Ding <dingyiheng91@gmail.com>
2023-02-23 21:30:10 -0500

    Finish header foundation

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleNav() {
+  const outterGrid = document.querySelector(".outer-grid");
+  outterGrid.classList.toggle("outer-expanded");
+  const hideNav = document.querySelector(".hidenav");
+  hideNav.classList.toggle("hidenav");
+}
